06adf61a4bad2d043cfa72d5e56bf74705e343f5
sanu4ka <sanu4ka1989@gmail.com>
2022-11-12 23:26:40 +0200

    1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,6 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
