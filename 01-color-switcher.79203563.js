e25aae7f7ef29f082477143b48dc1f7a3cde77e6
sanu4ka <37982710+sanu4ka@users.noreply.github.com>
2022-11-16 21:16:24 +0000

    Deploying to gh-pages from @ sanu4ka/goit-js-hw-09@4ab9aa09a6814830953103f77f1f94071f6eb151 🚀

---
+++ 01-color-switcher.79203563.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body"),n=null;e.addEventListener("click",(function(){n=setInterval((function(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),e.disabled=!0,t.disabled=!1}),1e3)})),t.addEventListener("click",(function(){clearInterval(n),e.disabled=!1,t.disabled=!0})),t.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.79203563.js.map
