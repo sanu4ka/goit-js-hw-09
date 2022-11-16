4ab9aa09a6814830953103f77f1f94071f6eb151
sanu4ka <sanu4ka1989@gmail.com>
2022-11-16 23:15:38 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,25 @@
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
+stopButton.disabled = true;
+
+function onStartButtonClick() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    startButton.disabled = true;
+    stopButton.disabled = false;
+  }, 1000);
+}
+
+function onStopButtonClick() {
+  clearInterval(timerId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
